e16e92380832004dba1e6ace8d48d697a2e7b87f
Ana-tb <ana_tc@comunidad.unam.mx>
2023-11-06 21:03:20 -0600

    First commit

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,65 @@
 const analyzer = {  
-  getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+//Recuento de palabras
+  getWordCount: (text) => { 
+    text = text.trim();
+    const words = text.split(/\s+/);
+    return words.length
   },
-  getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+
+  //Recuento de caracteres
+  getCharacterCount: (text) => {  
+    text = text.trim();
+    const characterCount = text.length;
+    return characterCount;
   },
-  getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-  },
+
   getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const regex = /\b[\w']+\b/g; //seleccionamos las letras y Ñs, estaba contando más palabras de las que debería
+    const words = text.match(regex);
+    
+    if (!words || words.length === 0) {
+      return 0;
+    } // ahora si habrán 0 palabras, asi que podrá haber un 0 de output
+    const sumChar = words.reduce((total,word)=> total + word.length,0);
+    const validChar = words.filter(word=>!/^\d+$/.test(word));
+
+    const averageLength = sumChar/validChar.length
+    return parseFloat(averageLength.toFixed(2)); // tofixed para redondear
   },
+
+  getCharacterCountExcludingSpaces: (text) => {
+    const cleanText = text.replace(/[^\w]/g, "");
+    const charcaterCountExcludingSpaces = cleanText.length;
+    return charcaterCountExcludingSpaces;
+  },
+ 
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const regex = /\b\d+(?:[.,]\d+)?\b/g;
+    const numbers = text.match(regex);
+    if(!numbers) {
+      return 0;
+    }
+    let numberCount = 0
+    for (let i = 0; i < numbers.length; i++) {
+      numberCount++;
+    }
+    return numberCount;
   },
   getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const regex = /[-+]?\b\d+\b(?:\.\d+)?/g;
+    const numbers = text.match(regex);
+    if (!numbers || numbers.length === 0) {
+      return 0;
+    }
+    let sum = 0;
+    numbers.forEach((num) => {
+      const parsedNum = parseFloat(num);
+      if (!isNaN(parsedNum)) {
+        sum += parsedNum;
+      }
+    });
+  
+    return sum;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
 import analyzer from './analyzer.js';
+//Llamar elementos del DOM 
+const textarea = document.querySelector("textarea[name='user-input']")
+const Limpiar = document.getElementById('reset-button')
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+
+//Eventos del DOM
+//Conteo de Caracteres 
+textarea.addEventListener('keyup', function () {
+  //Recuento de Palabras
+  const wordcount = document.querySelector("[data-testid='word-count']");
+  const contarPalabras = analyzer.getWordCount(textarea.value);
+  wordcount.textContent = `Recuento de palabras: ${contarPalabras}`
+ 
+  //Recuento de caracteres
+  const characterCount = document.querySelector("[data-testid='character-count']")
+  const contarCaracteres = analyzer.getCharacterCount(textarea.value);
+  characterCount.textContent = `Recuento de caracteres: ${contarCaracteres}`
+
+  //CARACTERES SIN ESPACIOS
+  const charcaterCountExcludingSpaces = document.querySelector("[data-testid='character-no-spaces-count']")
+  const caracteresSinEspacios = analyzer.getCharacterCountExcludingSpaces(textarea.value);
+  charcaterCountExcludingSpaces.textContent =`Recuento de caracteres excluyendo espacios y signos de puntuación: ${caracteresSinEspacios}`
+
+  //RECUENTO DE NÚMEROS
+  const numberCount = document.querySelector("[data-testid='number-count']")
+  const recuentodenumeros = analyzer.getNumberCount(textarea.value);
+  numberCount.textContent = `Recuento de números: ${recuentodenumeros}`
+
+  //SUMA TOTAL DE NÚMEROS 
+  const numberSum = document.querySelector("[data-testid='number-sum']")
+  const sumaNumeros = analyzer.getNumberSum(textarea.value);
+  numberSum.textContent = `Suma total de números: ${sumaNumeros}`
+
+  //LONGITUD MEDIA DE PALABRAS 
+  const averageWordLength = document.querySelector("[data-testid='word-length-average']")
+  const longitudmedia = analyzer.getAverageWordLength(textarea.value);
+  averageWordLength.textContent = `Longitud media de palabras: ${longitudmedia}`
+})
+
+//BOTON DE LIMPIAR 
+Limpiar.addEventListener('click', function() {
+  textarea.value = "";
+  //Recuento de Palabras
+  const wordcount = document.querySelector("[data-testid='word-count']");
+  wordcount.textContent = `Recuento de palabras: 0`
+  //Recuento de caracteres
+  const characterCount = document.querySelector("[data-testid='character-count']")
+  characterCount.textContent =`Recuento de caracteres: 0`
+  //CARACTERES SIN ESPACIOS
+  const charcaterCountExcludingSpaces = document.querySelector("[data-testid='character-no-spaces-count']")
+  charcaterCountExcludingSpaces.textContent = `Recuento de caracteres excluyendo espacios y signos de puntuación: 0`
+  //RECUENTO DE NÚMEROS
+  const numberCount = document.querySelector("[data-testid='number-count']")
+  numberCount.textContent = `Recuento de números: 0`
+  //SUMA TOTAL DE NÚMEROS 
+  const numberSum = document.querySelector("[data-testid='number-sum']")
+  numberSum.textContent = `Suma total de números: 0`
+  //LONGITUD MEDIA DE PALABRAS 
+  const averageWordLength = document.querySelector("[data-testid='word-length-average']")
+  averageWordLength.textContent =`Longitud media de palabras: 0`
+})
+
+
+
+    
+
+
